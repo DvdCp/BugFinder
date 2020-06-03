88cba15368b443a29b0b50ef067625cd8823f9d8
David <46301704+DvdCp@users.noreply.github.com>
2020-06-04 00:41:04 +0200

    Initial commit

---
+++ app/src/main/java/com/dcab/bugfinder/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dcab.bugfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
