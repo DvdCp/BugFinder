a4464cf7fdc3ff28c416cfbe0afbf1562c2bafd1
Alessandro <52054592+alessandro-bergamo@users.noreply.github.com>
2020-06-04 19:47:48 +0200

    First implementation of bug identification

--- app/src/main/java/com/dcab/bugfinder/MainActivity.java
+++ app/src/main/java/com/dcab/bugfinder/MainActivity.java
@@ -3,12 +3,15 @@ package com.dcab.bugfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -32,8 +35,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     {
         // Add a marker in Rome, Australia,
         // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(41.981807, 12.0819104);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng rome = new LatLng(41.981807, 12.0819104);
+
+        int height = 20;
+        int width = 20;
+        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.bug_rilevation);
+        Bitmap b = bitmapdraw.getBitmap();
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+
+        googleMap.addMarker(new MarkerOptions().position(rome).title("Marker in Rome").icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(rome));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
     }
 }
