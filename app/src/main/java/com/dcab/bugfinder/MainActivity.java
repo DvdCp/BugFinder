17156ff755c0571349c98527c38bdb9d66033680
Alessandro <52054592+alessandro-bergamo@users.noreply.github.com>
2020-06-04 19:06:41 +0200

    First Commit, first implementation homepage

--- app/src/main/java/com/dcab/bugfinder/MainActivity.java
+++ app/src/main/java/com/dcab/bugfinder/MainActivity.java
@@ -2,13 +2,37 @@ package com.dcab.bugfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_maps);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    public void onMapReady(GoogleMap googleMap)
+    {
+        // Add a marker in Rome, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(41.981807, 12.0819104);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
