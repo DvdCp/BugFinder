064ac7a4125f591570ea99a1862efbf9e28c4fea
David <46301704+dvdcp@users.noreply.github.com>
2020-06-06 23:03:29 +0200

    Scrollable Menu on Report.java

--- app/src/main/java/com/dcab/bugfinder/Report.java
+++ app/src/main/java/com/dcab/bugfinder/Report.java
@@ -1,23 +1,27 @@
 package com.dcab.bugfinder;
 
-
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.PopupMenu;
-
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
 
 public class Report extends AppCompatActivity
 {
-    Button specieButton;
-    LinearLayout formNewReport2;
-    ImageView sparisciTutto;
+    private ArrayAdapter arrayAdapter;
+    private ListView listView;
+    private ArrayList bugList;
+    private Button specieButton;
+    private LinearLayout formNewReportBottom;
+    private ImageView sparisciTutto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,39 +29,50 @@ public class Report extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_report);
 
+        // ----- lista di prova
+        listView = findViewById(R.id.bug_selection_list);
+        bugList = new ArrayList();
+        bugList.add("prova1");
+        bugList.add("prova2");
+        bugList.add("prova3");
+        bugList.add("prova4");
+        bugList.add("prova5");
+        bugList.add("prova6");
+
+        arrayAdapter = new ArrayAdapter(Report.this, R.layout.bug_list_item,R.id.item_tw,bugList);
+        listView.setAdapter(arrayAdapter);
+
         specieButton = findViewById(R.id.showSpecie);
-        formNewReport2 = findViewById(R.id.formNewReport2);
+        formNewReportBottom = findViewById(R.id.formNewReportBottom);
         sparisciTutto = findViewById(R.id.sparisciTutto);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                TextView text = (TextView) view.findViewById(R.id.item_tw);
+                specieButton.setText(text.getText());
+                sparisciTutto.callOnClick();
+            }
+        });
 
         specieButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                //Creating the instance of PopupMenu
-                PopupMenu popup = new PopupMenu(getApplicationContext(), specieButton);
-                //Inflating the Popup using xml file
-                popup.getMenuInflater().inflate(R.menu.specie_menu, popup.getMenu());
-                formNewReport2.setVisibility(View.GONE);
-                //registering popup with OnMenuItemClickListener
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
-                {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        specieButton.setText(item.getTitle().toString());
-                        formNewReport2.setVisibility(View.VISIBLE);
-                        return true;
-                    }
-                });
-
-                popup.show(); //showing popup menu
+                specieButton.setBackground(getDrawable(R.drawable.circle_shape3_no_border));
+                listView.setVisibility(View.VISIBLE);
+                formNewReportBottom.setVisibility(View.GONE);
             }
-        }); //closing the setOnClickListener method
-
+        });
 
         sparisciTutto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                formNewReport2.setVisibility(View.VISIBLE);
+                specieButton.setBackground(getDrawable(R.drawable.circle_shape3));
+                formNewReportBottom.setVisibility(View.VISIBLE);
+                listView.setVisibility(View.GONE);
             }
         });
     }
